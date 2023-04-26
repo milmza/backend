a67995578926f06fb0c77b2c337e7c3186b9ec60
milmza <milmza@palatino.com.ar>
2023-04-25 23:38:48 -0300

    3preentrega

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,45 +4,55 @@ import {addOneUser} from '../services/users.services.js';
 
 
 export async function signupUser(req,res){
-    const {email} = req.body
-    const user = await usersModel.find({email})
-    if(user.length!==0){
-        res.redirect("/errorSignup")
+    try {
+        const {email, password} = req.body
+        const user = await usersModel.find({email})
+        if(user.length!==0){
+            res.redirect("/errorSignup")
+        }
+        const hashNewPassword = await hashPassword(password)
+        const newUser = {...req.body, password:hashNewPassword}
+        //guardado del hash
+        await addOneUser(newUser)
+        console.log('user',newUser)
+        res.redirect('/login').json({newUser})
+    } catch (error) {
+        return error
     }
-    const hashNewPassword = await hashPassword(password)
-    const newUser = {...req.body, password:hashNewPassword}
-    //guardado del hash
-    const newUserBD = await addOneUser(newUser)
-    res.redirect('/login')
-    return newUserBD
 }
+
+export const cookies = []
 
 export async function loginUser(req,res){
     const {email, password} = req.body
     const user = await usersModel.find({email})
     if(user.length !==0){
-        const isPass = await comparePasswords(password, user.password)
+        const isPass = await comparePasswords(password, user[0].password)
         if(isPass){
-            const user = {
-                email: req.user.email,
-                first_name: req.user.first_name,
-                last_name: req.user.last_name,
-                age: req.user.age,
-                role: req.user.role
-            };
-        const token = generateToken(user)
-        console.log('token:', token)
-        res.cookie('token', token)
-        if(token){
-            if(req.user.role === 'Admin'){
-                res.redirect('/admin', {user})
+            // console.log(user)
+            for (const key in req.body) {
+                req.session[key] = req.body[key];
+            }
+            req.session.logged = true;
+            req.session.email = user[0].email;
+            req.session.first_name = user[0].first_name;
+            req.session.last_name = user[0].last_name;
+            req.session.age = user[0].age;
+            req.session.role = user[0].role;
+            const token = generateToken(user)
+            res.cookie('token', token)
+            if(token){
+                cookies.push(token)
+                if(user[0].role === "Admin"){
+                    res.redirect('/admin')
+                }else{
+                    res.redirect('/products')
+                }
             }else{
-                // res.redirect('')
-                res.redirect('/products', {user})
+                res.send('not authorized')
             }
         }else{
-            res.send('not authorized')
-        }
+            res.redirect('/errorLogin')
         }
     }else{
         res.redirect('/errorLogin')
